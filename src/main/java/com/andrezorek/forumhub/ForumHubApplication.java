cbfed4e0df1410396e8be0bd2e571c6e9964bb7a
Andre <andrezorek@gmail.com>
2025-01-07 17:54:32 -0300

    atualiza propriedades do projeto

--- src/main/java/com/andrezorek/forumhub/ForumHubApplication.java
+++ src/main/java/com/andrezorek/forumhub/ForumHubApplication.java
@@ -5,20 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ForumhubApplication {
+public class ForumHubApplication {
 
 	public static void main(String[] args) {
 
 		// Carrega as variáveis do .env antes de iniciar o Spring Boot
 		Dotenv dotenv = Dotenv.configure()
-				.directory("./") // Diretorio do .env (raiz do projeto)
+				.directory("./") // Diretório do .env (raiz do projeto)
 				.load();
 
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-		SpringApplication.run(ForumhubApplication.class, args);
+		SpringApplication.run(ForumHubApplication.class, args);
 	}
 
 }
